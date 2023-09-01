da7c5a5b0819cab268f1259b02564f07e2a7ce22
seusomelier <fefetodobom@hotmail.com>
2023-09-01 15:37:36 -0300

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const noBox = document.getElementById("noBox");
+
+const generateButtonPosition = (dimension) => {
+  let newDimension;
+  do {
+    newDimension =  Math.random() * (window[`inner${dimension}`]);
+  } while (newDimension >= window[`inner${dimension}`] - 100)
+  return newDimension;
+}
+
+noBox.addEventListener("mouseover", () => {
+  setTimeout(() => {
+    const newX = generateButtonPosition('Width');
+    const newY = generateButtonPosition('Height');
+    noBox.style.position = 'absolute';
+    noBox.style.left = `${newX}px`;
+    noBox.style.top = `${newY}px`;
+  },14545);
+});
